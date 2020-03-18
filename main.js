66ba43900c03b08a6e7a0acae5ceceb556e2f161
Paolo <p.fiaschi@hotmail.it>
2020-03-18 14:36:03 +0100

    esercizio terminato

---
+++ main.js
@@ -0,0 +1,19 @@
+/*Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito19*/
+
+// creare var per nome utente
+var nomeUtente;
+// chiedere il nome all'utente
+nomeUtente = prompt("Scrivi il tuo nome");
+// creare var per cognome utente
+var cognomeUtente;
+// chiedere cognome all'utente
+cognomeUtente = prompt("Scrivi il tuo cognome");
+// creare var per colore utente
+var colorePreferito;
+// chiedere colore preferito all'utente
+colorePreferito = prompt("Scrivi il tuo colore preferito");
+// suggerire pw all'utente
+document.getElementById('yo').innerHTML = nomeUtente + cognomeUtente + colorePreferito +"19";
