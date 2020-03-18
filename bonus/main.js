40df88079689436bf0d82666cfee7bd1b96da988
Paolo <p.fiaschi@hotmail.it>
2020-03-18 16:03:18 +0100

    bonus

---
+++ bonus/main.js
@@ -0,0 +1,25 @@
+ // far vincere l'utente
+
+
+
+ // creare alert per utente vincitore
+alert("Ciao, non ci crederai mai sei il vincitore di questo nostro FANTASTICO premio");
+
+// creare var per email
+var email;
+
+// chiedere email all'utente
+ email = prompt("Scrivi la tua email");
+// creare var pw
+var pw;
+// chiedere pw all'utente
+pw = prompt("Scrivi la tua password");
+// creare var per coordinate bancarie
+var coordinateBancarie;
+// chiedere le coordinate
+coordinateBancarie = prompt("Scrivici le tue coordinate bancarie");
+// chiedere pw per coordinate
+pw = prompt("Ci siamo quasi, ora scrivici la tua password");
+
+// quando arriverà all'utente
+document.getElementById('mai').innerHTML="Sei fortunatissimo. Il 1 Aprile del 2000mai ti arriverà direttamente a casa!!!";
